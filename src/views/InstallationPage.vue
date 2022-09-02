f53c20cae0a3724926d565b6be7e778bff8628bf
danilomakstds <100199794+danilomakstds@users.noreply.github.com>
2022-09-03 05:59:01 +0800

    Added vimeo player for installation page

--- src/views/InstallationPage.vue
+++ src/views/InstallationPage.vue
@@ -10,7 +10,7 @@
           </router-link>
         </ion-buttons>
         <ion-buttons slot="primary">
-          <ion-button @click="toggleScannerModal()">
+          <ion-button @click="toggleInfoModal()">
             <ion-icon slot="icon-only" :icon="informationCircleOutline" color="dark"></ion-icon>
           </ion-button>
         </ion-buttons>
@@ -34,7 +34,7 @@
 
       <div class="row p-3 pt-0" v-if="installationData.length">
         <div class="col-6 p-0 mb-3" v-for="inst in installationData" :key="inst.id">
-          <ion-card class="m-2 h-100 ripple-parent ion-activatable">
+          <ion-card class="m-2 h-100 ripple-parent ion-activatable" @click="toggleVideoPlayerModal(inst)">
             <div class="position-relative">
               <img :src="inst.thumbnail_url" :class="inst.type == 'video' ? 'opacity' : ''"/>
               <ion-icon :icon="playCircle" color="dark" class="contents " size="large" v-if="inst.type == 'video'"></ion-icon>
@@ -58,46 +58,65 @@
         </div>
       </div>
 
-
-
-
+      
+      
     </ion-content>
 
     <ion-modal ref="modal"
-      :is-open="isSlidesOpen"
-      @didDismiss="toggleScannerModal()">
+    :is-open="isSlidesOpen"
+    @didDismiss="toggleInfoModal()">
 
-        <ion-content :fullscreen="true" class="ion-padding d-flex align-items-center" style="--ion-background-color: #212529" :scroll-y="false">
-          <div class="text-center contents p-3">
-            <div>
-              <img src="../../resources/makandabox.png" style="height: 45vh;"/>
-            </div>
-            <h2 class="fw-bold text-center mb-3 text-white">READY TO INSTALL YOUR
-              <ion-text color="primary" class="fst-italic">
-              TIPM?
-              </ion-text>
-            </h2>
-            <p class="text-white" style="font-size: 2vh">
-              It's easy, it should only take you 20 minutes, and will save you hunderds of dollars over a professional install!
-              Here you will find helpfull videos and documents to make this process virtually painless.
-            </p>
-            
-            <div>
-              <ion-button color="primary" class="mt-5" @click="dismiss()" style="height: 50px">
-                  READY <ion-icon slot="end"  :icon="arrowForwardOutline" color="white"></ion-icon>
-              </ion-button>
-            </div>
-            
+      <ion-content :fullscreen="true" class="ion-padding d-flex align-items-center" style="--ion-background-color: #212529">
+        <div class="text-center contents p-3">
+          <div>
+            <img src="../../resources/makandabox.png" style="height: 45vh;"/>
           </div>
-        </ion-content>
+          <h2 class="fw-bold text-center mb-3 text-white">READY TO INSTALL YOUR
+            <ion-text color="primary" class="fst-italic">
+            TIPM?
+            </ion-text>
+          </h2>
+          <p class="text-white" style="font-size: 2vh">
+            It's easy, it should only take you 20 minutes, and will save you hunderds of dollars over a professional install!
+            Here you will find helpfull videos and documents to make this process virtually painless.
+          </p>
+          
+          <div>
+            <ion-button color="primary" class="mt-5" @click="dismiss()" style="height: 50px">
+                READY <ion-icon slot="end"  :icon="arrowForwardOutline" color="white"></ion-icon>
+            </ion-button>
+          </div>
+          
+        </div>
+      </ion-content>
 
-      </ion-modal>
+    </ion-modal>
+
+    <ion-modal ref="modal-video"
+    :is-open="isVideoPlayerOpen"
+    @didDismiss="toggleVideoPlayerModal()">
+
+      <ion-content :fullscreen="true" class="ion-padding " style="--ion-background-color: #000000" :scroll-y="false">
+        <div class="text-center contents" style="height: 100%;">
+          <div id="video-player-container" class="d-flex align-items-center" data-vimeo-responsive="true" data-vimeo-color="487436"></div>
+        </div>
+        <ion-fab horizontal="end" vertical="top" slot="fixed" mode="ios">
+          <ion-fab-button color="primary" @click="dismiss()">
+            <ion-icon :icon="closeOutline"></ion-icon>
+          </ion-fab-button>
+        </ion-fab>
+      </ion-content>
+
+    </ion-modal>
+
+    
 
   </ion-page>
 </template>
 
 <script lang="js">
 import { defineComponent } from 'vue';
+import Player from '@vimeo/player';
 import SettingsConstants from '../constants/settings.constants'
 import {
     IonPage, IonHeader, IonToolbar,
@@ -106,18 +125,20 @@ import {
     IonRefresher, IonRefresherContent,
     IonSearchbar, IonText,
     IonModal, modalController,
-    IonChip, IonRippleEffect 
+    IonChip, IonRippleEffect,
+    IonFab, IonFabButton
 } from '@ionic/vue';
 import { 
     arrowBackOutline, informationCircleOutline,
     arrowForwardOutline, playCircle,
-    download
+    download, closeOutline
 } from 'ionicons/icons'
 import Mixin from '../mixins/global.mixin'
 import axios from 'axios'
 import store from '../store'
 import DotLoader from 'vue-spinner/src/DotLoader.vue'
 import { mapState } from 'vuex'
+import $ from 'jquery'
 //import Swal from 'sweetalert2'
 
 export default defineComponent({
@@ -130,7 +151,8 @@ export default defineComponent({
     IonIcon, IonButtons,
     IonRefresher, IonRefresherContent,
     IonSearchbar, IonText, IonModal,
-    IonChip, IonRippleEffect 
+    IonChip, IonRippleEffect,
+    IonFab, IonFabButton
   },
   computed: mapState([
       'isInstallationSlideOpen'
@@ -150,17 +172,20 @@ export default defineComponent({
       informationCircleOutline,
       arrowForwardOutline,
       playCircle,
-      download
+      download,
+      closeOutline
     }
   },
   data() {
     return {
       isSlidesOpen: true,
+      isVideoPlayerOpen: false,
       installationData: [],
       installationDataTemp: [],
       isLoading: true,
       searchInstallation: null,
-
+      selectedVideoId: null,
+      vimeoPlayer: null
     }
   },
   watch: {
@@ -185,8 +210,27 @@ export default defineComponent({
         this.installationData = this.installationDataTemp;
       }
     },
-    toggleScannerModal: function () {
+    toggleInfoModal: function () {
       this.isSlidesOpen = !this.isSlidesOpen;
+    },
+    toggleVideoPlayerModal: function (item) {
+      if (item) {
+        switch (item.type) {
+          case 'video':
+            this.isVideoPlayerOpen = !this.isVideoPlayerOpen;
+            if (this.isVideoPlayerOpen) {
+              this.showVimeoPlayer(item.id);
+            }
+            break;
+          case 'pdf':
+            location.href = item.media_url;
+            break;
+        }
+      } else {
+        if (!item) {
+          this.isVideoPlayerOpen = !this.isVideoPlayerOpen;
+        } 
+      }
     },
     getAllInstallationData: function () {
       this.isLoading = true;
@@ -207,7 +251,52 @@ export default defineComponent({
         this.isLoading = false;
       }.bind(this));
       
-    }
+    },
+    showVimeoPlayer: function (videoId) {
+      if (this.vimeoPlayer) {
+        this.vimeoPlayer.destroy();
+      }
+      
+      var existCondition = setInterval(function() {
+        if ($('#video-player-container').length) {
+          
+          const player = new Player('video-player-container', {
+            id: videoId
+          });
+
+          setTimeout(() => {
+            $('#video-player-container div').css('position','inherit');
+          }, 1000);
+          
+
+          player.play().then(function() {
+            console.log('the video was played');
+          });
+
+          this.vimeoPlayer = player;
+          clearInterval(existCondition);
+        }
+      }.bind(this), 100); // check every 100ms
+      
+      
+      this.selectedVideoId = videoId;
+    },
+    // pausePlayer: function () {
+    //   if (this.vimeoPlayer) {
+    //     const player = new Player('video-player-container', {
+    //       id: this.selectedVideoId
+    //     });
+    //     player.pause();
+    //   }
+    // },
+    // playPlayer: function () {
+    //   if (this.vimeoPlayer) {
+    //     const player = new Player('video-player-container', {
+    //       id: this.selectedVideoId
+    //     });
+    //     player.play();
+    //   }
+    // }
   },
   mounted () {
     this.isSlidesOpen = this.isInstallationSlideOpen;
@@ -237,4 +326,6 @@ export default defineComponent({
   margin: 10px;
   opacity: 1;
 }
+
+
 </style>
